64f5952df2c8ff002633fdfc5e1f4ff08c4fc162
Valeriia <valeriia.air@gmail.com>
2024-02-10 21:38:43 +0100

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import MagicBallPage from "./pages/MagicBallPage/MagicBallPage";
@@ -10,15 +10,17 @@ import { SharedLayout } from "./components/SharedLayout/SharedLayout";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="ball" element={<MagicBallPage />} />
-        <Route path="quotes" element={<QuoteGeneratorPage />} />
-        <Route path="tic-tac-toe" element={<TicTacToePage />} />
+    <BrowserRouter basename="time-for-fun-react">
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="ball" element={<MagicBallPage />} />
+          <Route path="quotes" element={<QuoteGeneratorPage />} />
+          <Route path="tic-tac-toe" element={<TicTacToePage />} />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
