c19b9b2ac54bdab2f6d13a40dca1544c181ad3e3
Valeriia <valeriia.air@gmail.com>
2024-02-19 20:08:42 +0100

    box-shadow for NavLinks

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -2,18 +2,19 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const PageContainer = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-  background-color: rgba(84, 120, 80, 0.584);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 24px;
 `;
 
 export const NavContainer = styled.nav`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-  gap: 12px;
+  gap: 24px;
 
-  @media screen and (min-width: 769px) {
+  @media screen and (min-width: 992px) {
     grid-template-columns: repeat(2, 1fr);
   }
 `;
@@ -24,10 +25,17 @@ export const NavLinkStyled = styled(NavLink)`
 
   padding: 6px 24px;
 
-  border: 2px solid rgba(30, 95, 31, 0.854);
-  background-color: rgba(35, 54, 33, 0.584);
+  border: 2px solid rgba(30, 50, 30, 0.85);
+  border-radius: 6px;
+  background-color: rgba(25, 50, 25, 0.75);
 
+  box-shadow: 0px 6px 12px rgba(110, 175, 95, 0.5);
   text-decoration: none;
+
+  &:focus,
+  &:hover {
+    box-shadow: 0px 6px 24px rgb(185, 175, 30);
+  }
 `;
 
 export const ImageStyled = styled.img``;
@@ -36,7 +44,7 @@ export const TitleStyled = styled.h2`
   justify-self: self-end;
   align-self: self-end;
 
-  font-size: 32px;
+  font-size: 24px;
   font-weight: 600;
 
   text-shadow: 1px 1px 2px rgba(30, 95, 31, 0.854),
