2139107313a3b03be62d7c607ac3f4950aebdb55
Valeriia <valeriia.air@gmail.com>
2024-03-05 19:23:34 +0100

    themeProvider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 import { App } from "./App";
 
-import './styles/index.css';
+import "./styles/index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/time-for-fun-react">
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/time-for-fun-react">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/styles/theme.jsx
@@ -0,0 +1,99 @@
+/* // X-Small devices (portrait phones, less than 576px)
+// No media query for `xs` since this is the default in CoreUI
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) { ... }
+
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) { ... }
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) { ... }
+
+// X-Large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) { ... }
+
+// XX-Large devices (larger desktops, 1400px and up)
+@media (min-width: 1400px) { ... } */
+
+// padding: ${p => p.theme.space[4]}px ${p => p.theme.space[6] + 4}px;
+
+export const theme = {
+  breakpoints: {
+    mobile: '376px',
+    tablet: '768px',
+    desktop: '992px',
+    largeDesktop: '1400px',
+  },
+
+  space: [ 6, 8, 12, 24,],
+
+  borders: {
+    none: 'none',
+    normal: '',
+    medium: '',
+  },
+
+  radii: {
+
+  },
+
+  shadows: {
+
+  },
+
+  fonts: {
+    main: '',
+    secondary: '',
+  },
+
+  fontSizes: {
+    xxs: '',
+    xs: '',
+    s: '',
+    m: '',
+    l: '',
+    xl: '',
+
+    logo: '',
+    heading: '',
+  },
+
+  fontWeights: {
+    thin: 100,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+
+  lineHeights: {},
+
+  letterSpacings: {},
+
+  opacities: {},
+
+  sizes: {},
+
+  borderStyles: {},
+
+  borderWidths: {},
+
+  transitions: {},
+
+  zIndices: {},
+
+  colors: {
+    white: '#FFFFFF',
+    black: '#111111',
+
+    primary: '',
+    secondary: '',
+
+    btnMain: '',
+    btnAccent: '',
+  },
+};
